06f0bd66b2909ca8ec165ee68b23cd48db51265a
User <thetai6102@gmail.com>
2020-10-13 22:24:31 -0500

    Updates variables

---
+++ FundamentosJava/src/main/java/com/mycompany/fundamentosjava/Variables.java
@@ -0,0 +1,33 @@
+package com.mycompany.fundamentosjava;
+
+public class Variables {
+
+    public static void main(String args[]) {
+        System.out.println("Hola Mundo");
+
+        
+        //Tipos de datos
+        //Tipos primitivos
+        //Tipos Enteros
+        /**
+         * Definimos la variable primero declarando el tipo de dato que va a
+         * tener dentro mi variable
+         */
+        int miVariableEntera = 10;
+        System.out.println("Mi variable es: " + miVariableEntera);
+        String miVariableCadena = "Craig Kem Castro Pairazaman";
+        System.out.println("Mi variable de tipo cadena es:" + miVariableCadena);
+        /**
+         * Reutilizamos nuestra variable y le asignamos un nuevo valor
+         */
+        miVariableEntera = 20;
+        System.out.println("El nuevo valor de mi variable es:" + miVariableEntera);
+        miVariableCadena = "Rick Julcamoro Pairazaman";
+        System.out.println("El nuevo valor de mi variable de tipo cadena es:" + miVariableCadena);
+        //Tipos Flotantes
+        //Tipos referenciados
+        //Clases
+        //Interfaces
+        //Arreglos
+    }
+}
